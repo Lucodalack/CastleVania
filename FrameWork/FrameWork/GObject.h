a49c218784dd6e90793af6039de1f61138149b0c
Luco <tientin995@gmail.com>
2016-11-20 14:13:38 +0700

    add enum

--- FrameWork/FrameWork/GObject.h
+++ FrameWork/FrameWork/GObject.h
@@ -9,7 +9,45 @@
 #include "Box.h"
 #define GRAVITY			0.2f
 
-
+enum TypeGame {
+	Boss_Bat = 0,
+	Boss_Medusa = 1,
+	Boss_Simon = 2,
+	Enemy_Bat = 3,
+	Enemy_Bonepilla = 4,
+	Enemy_Ghost = 5,
+	Enemy_Medusahead = 6,
+	Enemy_Merman = 7,
+	Enemy_Panther = 8,
+	Enemy_Spearguar = 9,
+	Enemy_Zombie = 10,
+	Ground_Brick = 11,
+	Ground_Fireandle = 12,
+	Ground_Firetower = 13,
+	Ground_Go_In_Castle = 14,
+	Ground_Hidden = 15,
+	Ground_Lockdoor = 16,
+	Ground_Moving_Brick = 17,
+	Ground_Next = 18,
+	Ground_Opendoor = 19,
+	Ground_Stair_Down = 20,
+	Ground_Stair_Up = 21,
+	Ground_Trap = 22,
+	Item_axe = 23,
+	Item_cross = 24,
+	Item_knife = 25,
+	Item_holy_water = 26,
+	Item_stop_watch = 27,
+	Item_morning_star = 28,
+	Item_double_shot = 29,
+	Item_small_heart = 30,
+	Item_big_heart = 31,
+	Item_money_bag = 32,
+	Item_roast = 33,
+	Item_rosary = 34,
+	Item_spirit_ball = 35,
+	Item_none = 36
+};
 
 class GObject {
 protected:
