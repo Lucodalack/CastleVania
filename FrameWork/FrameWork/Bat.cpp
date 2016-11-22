7d1b89aca1aa8d60067c8a69b2be908b9cb050d8
minh đẹp trai <tavanminh97@gmail.com>
2016-11-22 11:26:58 +0700

    FULL ENEMY STATE 4

--- FrameWork/FrameWork/Bat.cpp
+++ FrameWork/FrameWork/Bat.cpp
@@ -2,18 +2,11 @@
 
 
 Bat::Bat(int x, int y, int x2, int y2) :
-GObject(TYPE, x, y, _WIDTH, _HEIGHT)
+Enemy(TYPE, x, y, x2, y2)
 {
-	_isMoveleft = false;
-	_isDead = false;
 	_isSleep = true;
-	_vy = GRAVITY;
-	_vx = _SPEED;
-	_box = Box(x, y, _WIDTH, _HEIGHT, _vx, _vy);
-	_activeArea.top = y;
-	_activeArea.left = x;
-	_activeArea.right = x2;
-	_activeArea.bottom = y2;
+	_vx = _batSPEED;
+	_box = Box(x, y, _batWIDTH, _batHEIGHT, _vx, _vy);
 	GTexture* texture = new GTexture(BAT_SPRITE, 4, 1, 4);
 	_sprite = new GSprite(texture, _ANIMATE_RATE);
 }
@@ -27,7 +20,7 @@ void Bat::MoveUpdate(float deltaTime)
 		return;
 	}
 	if (this->_y <= ynek){
-		_vy = 2*_SPEED;
+		_vy = 2*_batSPEED;
 		this->_y += int(deltaTime*_vy);
 		return; // con nay ddang roi xuong 1 doan cho bang thang simon roi moi bay
 	}
@@ -74,14 +67,6 @@ void Bat::Update(float deltatime){
 	this->_sprite->Update(deltatime);
 }
 
-void Bat::Draw(){
-	if (!this->_isMoveleft){
-		this->_sprite->DrawFlipX(_x, _y);
-	}
-	else{
-		this->_sprite->Draw(_x, _y);
-	}
-}
 void Bat::ChangeState(int state){
 	if (state == BATSATE::IsSleep){
 		this->_isSleep = true;
--- FrameWork/FrameWork/Spearguard.cpp
+++ FrameWork/FrameWork/Spearguard.cpp
@@ -1,46 +1,13 @@
 #include "Spearguard.h"
 
 
-
 Spearguard::Spearguard(int x, int y, int x2, int y2) :
-GObject(TYPE, x, y, _WIDTH, _HEIGHT)
+Enemy(TYPE, x, y, x2, y2)
 {
-	_isMoveleft = false;
-	_isDead = false;
-	_vy = GRAVITY;
-	_vx = _SPEED;
-	_box = Box(x, y, _WIDTH, _HEIGHT, _vx, _vy);
-	_activeArea.top = y;
-	_activeArea.left = x;
-	_activeArea.right = x2;
-	_activeArea.bottom = y2;
-	GTexture* texture = new GTexture(SPEARGUARD_SPRITE, 4, 1, 4);
-	_sprite = new GSprite(texture, _ANIMATE_RATE);
+	GTexture* tt = new GTexture(SPEARGAURD_SPRITE, 4, 1, 4);
+	_sprite = new GSprite(tt, _ANIMATE_RATE);
+	this->_vx = _guardSPEED;
 	this->SetFrame(0);
-}
-
-void Spearguard::MoveUpdate(float deltaTime)
-{
-#pragma region __XU_LY_CHUYEN_DONG__
-	if (_isMoveleft){
-		if (_x <= _activeArea.left){
-			_vx *= -1;
-			this->_isMoveleft = !this->_isMoveleft;
-		}
-	}
-	else{
-		if (_x >= _activeArea.right){
-			_vx *= -1;
-			this->_isMoveleft = !this->_isMoveleft;
-		}
-	}
-
-	this->_x += int(this->_vx * deltaTime);
-#pragma endregion
-	_box.x = _x;
-	_box.y = _y;
-	_box.vx = _vx;
-	_box.vy = _vy;
 }
 
 void Spearguard::SetFrame(float deltaTime)
@@ -51,19 +18,6 @@ void Spearguard::SetFrame(float deltaTime)
 #pragma endregion
 }
 
-void Spearguard::Update(float deltatime){
-	this->MoveUpdate(deltatime);
-	this->_sprite->Update(deltatime);
-}
-
-void Spearguard::Draw(){
-	if (!this->_isMoveleft){
-		this->_sprite->DrawFlipX(_x, _y);
-	}
-	else{
-		this->_sprite->Draw(_x, _y);
-	}
-}
 Spearguard::~Spearguard(){
 	if (_sprite != NULL){
 		delete _sprite;
