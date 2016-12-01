51c310db1a66ef28d28b0ba49501c57cfee4ebf2
minh đẹp trai <tavanminh97@gmail.com>
2016-12-01 10:15:32 +0700

    maged

--- FrameWork/FrameWork/Game.h
+++ FrameWork/FrameWork/Game.h
@@ -9,24 +9,9 @@
 #include "Bricks.h"
 #include "SweptAABB.h"
 #include <string>
-#include "Ghost.h"
-#include "Spearguard.h"
-#include "Bat.h"
-#include "Cross.h"
-#include "Axe.h"
-#include "Knife.h"
-#include "HolyWater.h"
-#include "StopWatch.h"
-#include "MorningStar.h"
-#include "Roast.h"
-#include "Rosary.h"
-#include "MoneyBag.h"
-#include "SpiritBall.h"
-#include "BigHeart.h"
-#include "SmallHeart.h"
-#include "DoubleShot.h"
-#include "ItemNone.h"
-#include "MedusaHead.h"
+#include "Quadtree.h"
+#include "GObject.h"
+#include <vector>
 using namespace std;
 
 #define SIMON_SPRITE L"simon.png"
@@ -39,13 +24,14 @@ class Game
 	GSprite* _mybackground;
 	Bricks* _bricks;
 	CSweptAABB* swepyAABB;
+	
 
 	GObject *_mbat, *_mspearguard, *_mghost,*_item,*_enemy;
 
 public:
 	static Game* getCurGame();
 
-
+	vector<GObject*> listObject;
 	void GameInit();
 	void GameLoad();
 	void GameRun(float deltatime);
